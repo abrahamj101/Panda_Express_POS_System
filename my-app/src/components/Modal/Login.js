b74aa4fa41b32bbffe4f2f1aa8d235026b9433bf
CreeKeem <388cjk@gmail.com>
2024-11-08 17:05:30 -0600

    fixed modal styling
    the path for the modal css file was wrong and fixed it

--- my-app/src/components/Modal/Login.js
+++ my-app/src/components/Modal/Login.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./../styles/Modal.css"; // Add styles for the modal
+import "../../styles/Modal.css"; // Add styles for the modal
 
-const Modal = ({ isOpen, onClose }) => {
+const Login = ({ isOpen, onClose }) => {
   if (!isOpen) return null;
 
   return (
@@ -23,4 +23,4 @@ const Modal = ({ isOpen, onClose }) => {
   );
 };
 
-export default Modal;
+export default Login;
