48bb2fdf0ea25d17a642425ecdf30ffd53aba7a8
CreeKeem <388cjk@gmail.com>
2024-11-08 01:38:25 -0600

    added better division of fooditem type
    there will be a header that lets the user know what the food item type is

--- my-app/src/components/FoodItems/FoodItemGrid.js
+++ my-app/src/components/FoodItems/FoodItemGrid.js
@@ -21,29 +21,39 @@ function FoodItemGrid({ foodItemIds }) {
 
   const currentMonth = new Date().getMonth() + 1;
 
+  const filteredFoodItems = foodItems
+    .filter(
+      (foodItem) =>
+        foodItemIds.includes(foodItem.fooditem_id) &&
+        foodItem.in_stock &&
+        foodItem.seasonal.includes(currentMonth)
+    )
+    .sort((a, b) => a.type.localeCompare(b.type));
+
+  const groupedFoodItems = filteredFoodItems.reduce((acc, foodItem) => {
+    const { type } = foodItem;
+    if (!acc[type]) acc[type] = [];
+    acc[type].push(foodItem);
+    return acc;
+  }, {});
+
   return (
     <Fragment>
-      <div className="item-grid">
-        {foodItems.length > 0 ? (
-          foodItems
-            .filter(
-              (foodItem) =>
-                foodItemIds.includes(foodItem.fooditem_id) &&
-                foodItem.in_stock &&
-                foodItem.seasonal.includes(currentMonth)
-            )
-            .map((foodItem) => (
+      {Object.keys(groupedFoodItems).map((type) => (
+        <div key={type}>
+          <h1 className='type'>{type}</h1>
+          <div className="item-grid">
+            {groupedFoodItems[type].map((foodItem) => (
               <FoodItemCard
                 key={foodItem.fooditem_id}
                 foodItem_name={foodItem.fooditem_name}
                 image_link={foodItem.image_link}
                 premium={foodItem.premium}
               />
-            ))
-        ) : (
-          <p>Loading food items...</p>
-        )}
-      </div>
+            ))}
+          </div>
+        </div>
+      ))}
     </Fragment>
   );
 }
