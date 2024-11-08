ea53dbdb0ff275c28915bf505958cb1b1f855452
CreeKeem <388cjk@gmail.com>
2024-11-07 23:39:43 -0600

    made it so that the user will be able to go home by pressing the panda logo
    edited the header to make the logo go back to home

--- my-app/src/components/MenuItems/MenuItemCard.js
+++ my-app/src/components/MenuItems/MenuItemCard.js
@@ -1,9 +1,11 @@
 const MenuItemCard = ({ menuItem_name, price, image_link }) => {
     return (
       <div className="menu-item-card">
-        <img src={image_link} alt={menuItem_name} className="menu-item-image" />
-        <h3>{menuItem_name}</h3>
-        <p>Price: ${price}</p> {/* Ensures price is displayed with two decimals */}
+        <button>
+            <img src={image_link} alt={menuItem_name} className="menu-item-image" />
+            <h3>{menuItem_name}</h3>
+            <p>Price: ${price}</p>
+        </button>
       </div>
     );
   }
--- my-app/src/components/Navigation/Header.js
+++ my-app/src/components/Navigation/Header.js
@@ -1,12 +1,15 @@
 import logo from "../../images/panda-express-logo-1.svg";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
 
 const Header = () => {
     return (
         <header className="header">
-        <div className="logo">
-          <img src={logo} alt="Panda Express Logo" />
-        </div>
+          <Link to="/">
+            <div className="logo">
+              <img src={logo} alt="Panda Express Logo" />
+            </div>
+          </Link>
         <h1>Panda Express</h1>
         <button className="login-btn">Login</button>
       </header>
