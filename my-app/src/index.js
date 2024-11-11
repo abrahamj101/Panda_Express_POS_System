7c9223a1eecca83b147d1ddf0fe132fa54140691
CreeKeem <388cjk@gmail.com>
2024-11-11 15:32:12 -0600

    merging to main
    there are some errors we are looking to fix

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,6 +4,7 @@ import "./styles/index.css";
 import App from "./pages/App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
