025cd94aa84ba7b66d56a3f43ba7227187d32673
4M4Z3 <robert.tonylongo@gmail.com>
2024-11-21 14:27:29 -0600

    Merged robdev3 with recent main pull

--- my-app/src/components/MenuItems/MenuItemCard.js
+++ my-app/src/components/MenuItems/MenuItemCard.js
@@ -27,31 +27,3 @@ const MenuItemCard = ({ menuItem_name, price, image_link, foodItem_ids, menuItem
 
 export default MenuItemCard;
 
-import { Link } from "react-router-dom";
-import "../../styles/FoodandMenu/Cards.css";
-
-const MenuItemCard = ({ menuItem_name, price, image_link, foodItem_ids, menuItem_id, inventoryItemIds, inStock }) => {
-    
-    return (
-      <div className="item-card">
-        <Link
-          to="/food"
-          state={{
-            foodItem_ids,
-            menuItem_id,
-            menuItem_name,
-            price,
-            image_link,
-            inventoryItemIds, 
-            inStock,
-          }}
-        >
-          <img src={image_link} alt={menuItem_name} className="item-image" />
-          <h3>{menuItem_name}</h3>
-          <p>Price: ${price}</p>
-        </Link>
-      </div>
-    );
-};
-
-export default MenuItemCard;
