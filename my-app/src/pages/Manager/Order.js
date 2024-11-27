36e99d1adc042b34f44d6ce8ed9504b27dd353ae
Joshua Abraham <141776519+abrahamj101@users.noreply.github.com>
2024-11-27 02:20:45 -0600

    fixed order history in manager page so that it is the same as order history from the navbar

--- my-app/src/pages/Manager/Order.js
+++ my-app/src/pages/Manager/Order.js
@@ -1,17 +1,45 @@
+// import Header from "../../components/Navigation/Header";
+// import Footer from "../../components/Navigation/Footer";
+// import ManagerTable from "../../components/ManagerPage/ManagerTable";
+// import { useZoom } from "../../components/Zoom/ZoomContext";
+
+// function OrderPage() {
+//   const { zoomLevel } = useZoom();
+//   return (
+//     <div
+//       className="employeePage"
+//       style={{ transform: `scale(${zoomLevel})`, transformOrigin: "top left" }}
+//     >
+//       <Header />
+//       <ManagerTable dataType="order" />
+//       <Footer />
+//     </div>
+//   );
+// }
+
+// export default OrderPage;
+
+import React from "react";
+import "../../styles/Pages/default.css";
 import Header from "../../components/Navigation/Header";
 import Footer from "../../components/Navigation/Footer";
-import ManagerTable from "../../components/ManagerPage/ManagerTable";
 import { useZoom } from "../../components/Zoom/ZoomContext";
+import Customer from "../../components/OrderHistory/customer";
+import Cashier from "../../components/OrderHistory/cashier";
 
 function OrderPage() {
   const { zoomLevel } = useZoom();
+  const customer = true; // Adjust this based on logic to identify the user's role.
+
   return (
     <div
-      className="employeePage"
+      className="order-history"
       style={{ transform: `scale(${zoomLevel})`, transformOrigin: "top left" }}
     >
       <Header />
-      <ManagerTable dataType="order" />
+      <div className="main-content">
+        {customer ? <Customer /> : <Cashier />}
+      </div>
       <Footer />
     </div>
   );
