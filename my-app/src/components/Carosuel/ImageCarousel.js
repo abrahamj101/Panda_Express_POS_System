6ba2f79d2b525fe46b8dbeac5884be49eaabc397
Nathan Tserng <ntserng@gmail.com>
2024-12-02 13:23:53 -0600

    fixed image carousel

--- my-app/src/components/Carosuel/ImageCarousel.js
+++ my-app/src/components/Carosuel/ImageCarousel.js
@@ -9,9 +9,9 @@ const carouselItems = [
     legend: "Orange Chicken",
   },
   {
-    src: "https://nomnom-files.pandaexpress.com/global/assets/modifiers/Beef_BeijingBeef.png",
-    alt: "Beijing Beef",
-    legend: "Beijing Beef",
+    src: "https://nomnom-files.pandaexpress.com/global/assets/modifiers/Beef_ShanghaiAngusSteak.png",
+    alt: "Black Pepper Sirloin Steak",
+    legend: "Black Pepper Sirloin Steak",
   },
   {
     src: "https://nomnom-files.pandaexpress.com/global/assets/modifiers/Seafood_HoneyWalnutShrimp.png",
@@ -19,14 +19,54 @@ const carouselItems = [
     legend: "Honey Walnut Shrimp",
   },
   {
+    src: "https://nomnom-files.pandaexpress.com/global/assets/modifiers/Chicken_GrilledTeriyakiChicken.png",
+    alt: "Grilled Teriyaki Chicken",
+    legend: "Grilled Teriyaki Chicken",
+  },
+  {
+    src: "https://nomnom-files.pandaexpress.com/global/assets/modifiers/Chicken_KungPaoChicken.png",
+    alt: "Kung Pao Chicken",
+    legend: "Kung Pao Chicken",
+  },
+  {
+    src: "https://nomnom-files.pandaexpress.com/global/assets/modifiers/ChickenBreast_HoneySesameChickenBreast.png",
+    alt: "Honey Sesame Chicken Breast",
+    legend: "Honey Sesame Chicken Breast",
+  },
+  {
+    src: "https://nomnom-files.pandaexpress.com/global/assets/modifiers/Beef_BeijingBeef.png",
+    alt: "Beijing Beef",
+    legend: "Beijing Beef",
+  },
+  {
     src: "https://nomnom-files.pandaexpress.com/global/assets/modifiers/Chicken_MushroomChicken.png",
     alt: "Mushroom Chicken",
     legend: "Mushroom Chicken",
   },
   {
-    src: "https://nomnom-files.pandaexpress.com/global/assets/modifiers/Beef_ShanghaiAngusSteak.png",
-    alt: "Black Pepper Sirloin Steak",
-    legend: "Black Pepper Sirloin Steak",
+    src: "https://nomnom-files.pandaexpress.com/global/assets/modifiers/ChickenBreast_SweetFireChickenBreast.png",
+    alt: "SweetFire Chicken Breast",
+    legend: "SweetFire Chicken Breast",
+  },
+  {
+    src: "https://nomnom-files.pandaexpress.com/global/assets/modifiers/ChickenBreast_StringBeanChickenBreast.png",
+    alt: "String Bean Chicken Breast",
+    legend: "String Bean Chicken Breast",
+  },
+  {
+    src: "https://nomnom-files.pandaexpress.com/global/assets/modifiers/Beef_BroccoliBeef.png",
+    alt: "Broccoli Beef",
+    legend: "Broccoli Beef",
+  },
+  {
+    src: "https://nomnom-files.pandaexpress.com/global/assets/modifiers/Chicken_BlackPepperChicken.png",
+    alt: "Black Pepper Chicken",
+    legend: "Black Pepper Chicken",
+  },
+  {
+    src: "https://nomnom-files.pandaexpress.com/global/assets/modifiers/Vegetables_SuperGreens.png",
+    alt: "Super Greens",
+    legend: "Super Greens",
   },
 ];
 
@@ -52,11 +92,7 @@ function ImageCarousel() {
       >
         {carouselItems.map((item, index) => (
           <div className="image-box" key={index}>
-            <img
-              className="carousel-image"
-              src={item.src}
-              alt={item.alt}
-            />
+            <img className="carousel-image" src={item.src} alt={item.alt} />
             <p className="legend" aria-hidden="true">
               {item.legend}
             </p>
