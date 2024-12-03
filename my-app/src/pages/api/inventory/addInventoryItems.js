3a7080a12b18e8ebcaba2473ab9daff2dc9e487a
Roshan Sreedhar <r.sreedhar13@gmail.com>
2024-12-02 18:22:39 -0600

    updated inventory functions

---
+++ my-app/src/pages/api/inventory/addInventoryItems.js
@@ -0,0 +1,22 @@
+const addInventoryItem = async (item) => {
+    try {
+      const response = await fetch("https://project-3-team-3-b-backend.vercel.app/api/inventoryItems", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(item),
+      });
+      if (!response.ok) {
+        throw new Error("Failed to add inventory item");
+      }
+      const jsonData = await response.json();
+      return jsonData;
+    } catch (err) {
+      console.error(err.message);
+      throw err;
+    }
+  };
+  
+  export default addInventoryItem;
+  
