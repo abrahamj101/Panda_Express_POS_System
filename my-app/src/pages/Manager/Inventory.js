35e22da3fe100e6c0bbf364b62d61743ba5f68be
Roshan Sreedhar <r.sreedhar13@gmail.com>
2024-12-03 10:13:32 -0600

    add inventory button created

--- my-app/src/pages/Manager/Inventory.js
+++ my-app/src/pages/Manager/Inventory.js
@@ -1,21 +1,38 @@
-const addInventoryItem = async (item) => {
-  try {
-    const response = await fetch("https://project-3-team-3-b-backend.vercel.app/api/inventoryItems", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(item),
-    });
-    if (!response.ok) {
-      throw new Error("Failed to add inventory item");
-    }
-    const jsonData = await response.json();
-    return jsonData;
-  } catch (err) {
-    console.error(err.message);
-    throw err;
-  }
-};
+import Header from "../../components/Navigation/Header";
+import Footer from "../../components/Navigation/Footer";
+import ManagerTable from "../../components/ManagerPage/ManagerTable";
+import { useZoom } from "../../components/Zoom/ZoomContext";
+import BackButton from "../../components/Navigation/BackButton";
+import "../../styles/Pages/Inventory.css";
+import { Link } from "react-router-dom";
 
-export default addInventoryItem;
+function InventoryPage() {
+  const { zoomLevel } = useZoom();
+
+  const handleAddItemClick = () => {
+    // implement this next
+    console.log("Add Item button clicked");
+  };
+
+  return (
+    <div
+      className="employeePage"
+      style={{ transform: `scale(${zoomLevel})`, transformOrigin: "top left" }}
+    >
+      <Header />
+      <div className="main-content">
+        <BackButton location="/manager" />
+        <div className="inventory-header">
+          <h2>Inventory List</h2>
+          <button className="add-item-button" onClick={handleAddItemClick}>
+            Add Items
+          </button>
+        </div>
+        <ManagerTable dataType="inventory" />
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default InventoryPage;
