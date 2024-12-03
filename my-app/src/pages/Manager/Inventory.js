327bf933c61bc3e07f193161199b1478e71faa9c
CreeKeem <388cjk@gmail.com>
2024-12-03 12:11:38 -0600

    Revert "Merge branch 'roshanDev2' of https://github.com/CSCE331-Fall2024/project-3-team-3B into roshanDev4"
    This reverts commit 42539cea857c2baa1ea093cb2c781f184c98fc61, reversing
    changes made to ae5fd3b7c71da4c0afc0a2f2e8884b6c88a9a756.

--- my-app/src/pages/Manager/Inventory.js
+++ my-app/src/pages/Manager/Inventory.js
@@ -3,17 +3,9 @@ import Footer from "../../components/Navigation/Footer";
 import ManagerTable from "../../components/ManagerPage/ManagerTable";
 import { useZoom } from "../../components/Zoom/ZoomContext";
 import BackButton from "../../components/Navigation/BackButton";
-import "../../styles/Pages/Inventory.css";
-import { Link } from "react-router-dom";
 
 function InventoryPage() {
   const { zoomLevel } = useZoom();
-
-  const handleAddItemClick = () => {
-    // implement this next
-    console.log("Add Item button clicked");
-  };
-
   return (
     <div
       className="employeePage"
@@ -22,12 +14,6 @@ function InventoryPage() {
       <Header />
       <div className="main-content">
         <BackButton location="/manager" />
-        <div className="inventory-header">
-          <h2>Inventory List</h2>
-          <button className="add-item-button" onClick={handleAddItemClick}>
-            Add Items
-          </button>
-        </div>
         <ManagerTable dataType="inventory" />
       </div>
       <Footer />
