d701f82fed7cdd4425f84f1043a1d3516f9381bd
Roshan Sreedhar <r.sreedhar13@gmail.com>
2024-12-02 18:22:39 -0600

    updated inventory functions

--- my-app/src/pages/Manager/Inventory.js
+++ my-app/src/pages/Manager/Inventory.js
@@ -1,24 +1,21 @@
-import Header from "../../components/Navigation/Header";
-import Footer from "../../components/Navigation/Footer";
-import ManagerTable from "../../components/ManagerPage/ManagerTable";
-import { useZoom } from "../../components/Zoom/ZoomContext";
-import BackButton from "../../components/Navigation/BackButton";
+const addInventoryItem = async (item) => {
+  try {
+    const response = await fetch("https://project-3-team-3-b-backend.vercel.app/api/inventoryItems", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(item),
+    });
+    if (!response.ok) {
+      throw new Error("Failed to add inventory item");
+    }
+    const jsonData = await response.json();
+    return jsonData;
+  } catch (err) {
+    console.error(err.message);
+    throw err;
+  }
+};
 
-function InventoryPage() {
-  const { zoomLevel } = useZoom();
-  return (
-    <div
-      className="employeePage"
-      style={{ transform: `scale(${zoomLevel})`, transformOrigin: "top left" }}
-    >
-      <Header />
-      <div className="main-content">
-        <BackButton location="/manager" />
-        <ManagerTable dataType="inventory" />
-      </div>
-      <Footer />
-    </div>
-  );
-}
-
-export default InventoryPage;
+export default addInventoryItem;
