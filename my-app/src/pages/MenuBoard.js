fa92bf07cbffe3b404ec8f595bff079a0e8498bf
Nathan Tserng <ntserng@gmail.com>
2024-12-04 00:25:06 -0600

    added sides and entrees for menu board

--- my-app/src/pages/MenuBoard.js
+++ my-app/src/pages/MenuBoard.js
@@ -4,29 +4,29 @@ import "../styles/Pages/MenuBoard.css";
 const MenuPage = () => {
   return (
     <div className="menu-page">
-      {/* Take-Out Meals Section */}
+      {/* Menu Items Meals Section */}
       <div className="menu-section">
-        <h2 className="section-title">Take-Out Meals</h2>
+        <h2 className="section-title">Menu Items</h2>
         <div className="meal-options">
           <div className="meal-item">
             <h3>Bowl</h3>
             <p>1 Entrée & 1 Side</p>
-            <p className="price">$6.40</p>
+            <p className="price">$8.30</p>
           </div>
           <div className="meal-item">
             <h3>Plate</h3>
             <p>2 Entrées & 1 Side</p>
-            <p className="price">$7.40</p>
+            <p className="price">$9.80</p>
           </div>
           <div className="meal-item">
             <h3>Bigger Plate</h3>
             <p>3 Entrées & 1 Side</p>
-            <p className="price">$8.90</p>
+            <p className="price">$11.30</p>
           </div>
           <div className="meal-item">
-            <h3>Kid's Meal</h3>
+            <h3>Panda Cub Meal</h3>
             <p>Jr. Entrée, Jr. Side, 12 oz Drink & Cookie</p>
-            <p className="price">$5.50</p>
+            <p className="price">$6.60</p>
           </div>
         </div>
       </div>
@@ -36,23 +36,32 @@ const MenuPage = () => {
         <h2 className="section-title">Side Choices</h2>
         <div className="menu-grid">
           <div className="menu-item">
-            <img src="https://via.placeholder.com/150" alt="Chow Mein" />
+            <img
+              src="https://nomnom-files.pandaexpress.com/global/assets/modifiers/Sides_ChowMein.png"
+              alt="Chow Mein"
+            />
             <h3>Chow Mein</h3>
-            <p className="calories">510 Cal</p>
-          </div>
-          <div className="menu-item">
-            <img src="https://via.placeholder.com/150" alt="Fried Rice" />
-            <h3>Fried Rice</h3>
-            <p className="calories">520 Cal</p>
-          </div>
-          <div className="menu-item">
-            <img src="https://via.placeholder.com/150" alt="Mixed Vegetables" />
-            <h3>Mixed Vegetables</h3>
-            <p className="calories">90 Cal</p>
+            <p className="calories">600 Cal</p>
           </div>
           <div className="menu-item">
             <img
-              src="https://via.placeholder.com/150"
+              src="https://nomnom-files.pandaexpress.com/global/assets/modifiers/Sides_FriedRice.png"
+              alt="Fried Rice"
+            />
+            <h3>Fried Rice</h3>
+            <p className="calories">620 Cal</p>
+          </div>
+          <div className="menu-item">
+            <img
+              src="https://nomnom-files.pandaexpress.com/global/assets/modifiers/Vegetables_SuperGreens.png"
+              alt="Super Greens"
+            />
+            <h3>Super Greens</h3>
+            <p className="calories">130 Cal</p>
+          </div>
+          <div className="menu-item">
+            <img
+              src="https://nomnom-files.pandaexpress.com/global/assets/modifiers/Sides_WhiteSteamedRice.png"
               alt="White Steamed Rice"
             />
             <h3>White Steamed Rice</h3>
@@ -66,33 +75,108 @@ const MenuPage = () => {
         <h2 className="section-title">Entrée Choices</h2>
         <div className="menu-grid">
           <div className="menu-item">
-            <img src="https://via.placeholder.com/150" alt="Orange Chicken" />
+            <img
+              src="https://nomnom-files.pandaexpress.com/global/assets/modifiers/Chicken_OrangeChicken.png"
+              alt="Orange Chicken"
+            />
             <h3>Orange Chicken</h3>
-            <p className="calories">490 Cal</p>
+            <p className="calories">510 Cal</p>
           </div>
           <div className="menu-item">
             <img
-              src="https://via.placeholder.com/150"
-              alt="Beef with Broccoli"
+              src="https://nomnom-files.pandaexpress.com/global/assets/modifiers/Beef_ShanghaiAngusSteak.png"
+              alt="Black Pepper Sirloin Steak"
             />
-            <h3>Beef with Broccoli</h3>
+            <h3>Black Pepper Sirloin Steak</h3>
+            <p className="calories">180 Cal</p>
+          </div>
+          <div className="menu-item">
+            <img
+              src="https://nomnom-files.pandaexpress.com/global/assets/modifiers/Seafood_HoneyWalnutShrimp.png"
+              alt="Honey Walnut Shrimp"
+            />
+            <h3>Honey Walnut Shrimp</h3>
+            <p className="calories">430 Cal</p>
+          </div>
+          <div className="menu-item">
+            <img
+              src="https://nomnom-files.pandaexpress.com/global/assets/modifiers/Chicken_GrilledTeriyakiChicken.png"
+              alt="Grilled Teriyaki Chicken"
+            />
+            <h3>Grilled Teriyaki Chicken</h3>
+            <p className="calories">275 Cal</p>
+          </div>
+          <div className="menu-item">
+            <img
+              src="https://nomnom-files.pandaexpress.com/global/assets/modifiers/Chicken_KungPaoChicken.png"
+              alt="Kung Pao Chicken"
+            />
+            <h3>Kung Pao Chicken</h3>
+            <p className="calories">320 Cal</p>
+          </div>
+          <div className="menu-item">
+            <img
+              src="https://nomnom-files.pandaexpress.com/global/assets/modifiers/ChickenBreast_HoneySesameChickenBreast.png"
+              alt="Honey Sesame Chicken Breast"
+            />
+            <h3>Honey Sesame Chicken Breast</h3>
+            <p className="calories">340 Cal</p>
+          </div>
+          <div className="menu-item">
+            <img
+              src="https://nomnom-files.pandaexpress.com/global/assets/modifiers/Beef_BeijingBeef.png"
+              alt="Beijing Beef"
+            />
+            <h3>Beijing Beef</h3>
+            <p className="calories">480 Cal</p>
+          </div>
+          <div className="menu-item">
+            <img
+              src="https://nomnom-files.pandaexpress.com/global/assets/modifiers/Chicken_MushroomChicken.png"
+              alt="Mushroom Chicken"
+            />
+            <h3>Mushroom Chicken</h3>
+            <p className="calories">480 Cal</p>
+          </div>
+          <div className="menu-item">
+            <img
+              src="https://nomnom-files.pandaexpress.com/global/assets/modifiers/ChickenBreast_SweetFireChickenBreast.png"
+              alt="SweetFire Chicken Breast"
+            />
+            <h3>SweetFire Chicken Breast</h3>
+            <p className="calories">380 Cal</p>
+          </div>
+          <div className="menu-item">
+            <img
+              src="https://nomnom-files.pandaexpress.com/global/assets/modifiers/ChickenBreast_StringBeanChickenBreast.png"
+              alt="String Bean Chicken Breast"
+            />
+            <h3>String Bean Chicken Breast</h3>
+            <p className="calories">210 Cal</p>
+          </div>
+          <div className="menu-item">
+            <img
+              src="https://nomnom-files.pandaexpress.com/global/assets/modifiers/Beef_BroccoliBeef.png"
+              alt="Broccoli Beef"
+            />
+            <h3>Broccoli Beef</h3>
             <p className="calories">150 Cal</p>
           </div>
           <div className="menu-item">
             <img
-              src="https://via.placeholder.com/150"
-              alt="Honey Walnut Shrimp"
+              src="https://nomnom-files.pandaexpress.com/global/assets/modifiers/Chicken_BlackPepperChicken.png"
+              alt="Black Pepper Chicken"
             />
-            <h3>Honey Walnut Shrimp</h3>
-            <p className="calories">360 Cal</p>
+            <h3>Black Pepper Chicken</h3>
+            <p className="calories">280 Cal</p>
           </div>
           <div className="menu-item">
             <img
-              src="https://via.placeholder.com/150"
-              alt="Grilled Teriyaki Chicken"
+              src="https://nomnom-files.pandaexpress.com/global/assets/modifiers/Vegetables_SuperGreens.png"
+              alt="Super Greens"
             />
-            <h3>Grilled Teriyaki Chicken</h3>
-            <p className="calories">300 Cal</p>
+            <h3>Super Greens</h3>
+            <p className="calories">130 Cal</p>
           </div>
         </div>
       </div>
